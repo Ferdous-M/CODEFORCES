455f534cae1ae6ddb4787bc9fc0954d7aae7272a
Ferdous-M <137258398+Ferdous-M@users.noreply.github.com>
2024-06-12 18:13:44 +0600

    Create maxium multiple sum.cpp

---
+++ DIV 4/B/maxium multiple sum.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    int n;
+    cin>>n;
+    if(n==3){
+        cout<<3;
+    }
+    else{
+        cout<<2;
+    }
+    cout<<endl;
+ 
+}
+int main(){
+ 
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
